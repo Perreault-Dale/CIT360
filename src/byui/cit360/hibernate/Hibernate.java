ea91452a36be007021afbde5108930c40d19b3d9
Dale Perreault <per15038@byui.edu>
2018-02-01 08:08:18 -0600

    REsolving issues with Hibernate

--- src/byui/cit360/hibernate/Hibernate.java
+++ src/byui/cit360/hibernate/Hibernate.java
@@ -11,6 +11,9 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,5 +22,33 @@ import org.hibernate.cfg.Configuration;
 public class Hibernate {
     private static SessionFactory factory;
     
+    protected static void setUp() throws Exception {
+        factory = new Configuration().configure().buildSessionFactory();
+    }
     
+    public static void saveRecords(Set<Employee> es) {
+        try {
+            setUp();
+        } catch (Exception ex) {
+            Logger.getLogger(Hibernate.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Session session = factory.openSession();
+        session.beginTransaction();
+        es.stream().forEach((e) -> {
+            session.save(e);
+        });
+        session.getTransaction().commit();
+        session.close();
+        System.out.println("Records stored successfully.");
+    }
+    
+    public static void getRecords() {
+        Session session = factory.openSession();
+        session.beginTransaction();
+        List<Employee> el = session.createQuery("from employee").list();
+        System.out.println("DB Query Results:");
+        for (Employee e : el) {
+            System.out.println(e.getEmpNumber() + ", " + e.getFirstName() + " " + e.getLastName());
+        }
+    }
 }
