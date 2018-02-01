4a017fa59d03707a41f9fa50728f1a9a7e5ad6d6
Dale Perreault <per15038@byui.edu>
2018-01-31 22:56:21 -0600

    Added code for Hibernate

---
+++ src/byui/cit360/hibernate/Hibernate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit360.hibernate;
+
+import byui.cit360.collections.model.Employee;
+import org.hibernate.HibernateException; 
+import org.hibernate.Session; 
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author Dale
+ */
+public class Hibernate {
+    private static SessionFactory factory;
+    
+    
+}
