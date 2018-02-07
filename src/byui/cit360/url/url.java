dfb937dc6ccb157ee84aba074ccf8467e546b64d
Dale Perreault <per15038@byui.edu>
2018-02-07 08:25:30 -0600

    Completed URLConnection method with sending and receiving JSON

--- src/byui/cit360/url/url.java
+++ src/byui/cit360/url/url.java
@@ -10,18 +10,36 @@ import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import javax.net.ssl.HttpsURLConnection;
+import org.json.simple.parser.ParseException;
 
 /**
  *
  * @author Dale
  */
 public class url {
-    static Integer acctNum = 123456;
-    static String userName = "bossman";
-    static String apiKey = "ehf9phf1jhefpfhj1epfhc1078y14r3fh";
-    String apiToken;
+    static Integer acctNum = null;
+    static String userName = null;
+    static String apiKey = null;
+    static String apiToken = null;
     
     public static void getToken() {
+        // Get input from user
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter User Name: ");
+        try {
+            userName = reader1.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(url.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.print("Enter API Key: ");
+        try {
+            apiKey = reader1.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(url.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         // Create inner JSON with credentials
         JSONObject creds = new JSONObject();
         creds.put("username",userName);
@@ -34,8 +52,8 @@ public class url {
         // Create final JSON entry
         JSONObject authObj = new JSONObject();
         authObj.put("auth", authType);
-        
-        JSONObject response = new JSONObject();
+        JSONParser jParse = new JSONParser();
+        String response = null;
         
         // Print as a test
         System.out.println(authObj);
@@ -43,20 +61,23 @@ public class url {
         try {
             // Now create a URL connection
             URL identURL = new URL("https://identity.api.rackspacecloud.com/v2.0/tokens");
-            URLConnection connect = identURL.openConnection();
+            HttpsURLConnection connect = (HttpsURLConnection) identURL.openConnection();
             connect.setRequestProperty("Content-type", "application/json");
+            connect.setRequestMethod("POST");
             connect.setDoOutput(true);
+            connect.setDoInput(true);
             
             // Send the JSON with credentials
             OutputStreamWriter out = new OutputStreamWriter(connect.getOutputStream());
-            out.write(authObj.toJSONString());
+            out.write(authObj.toString());
             out.close();
             
+            // Print the response code from the server
+            System.out.println("Response from server: " + connect.getResponseCode() + " " + connect.getResponseMessage());
+            
             // Get the response
-            BufferedReader in = new BufferedReader(
-                                    new InputStreamReader(
-                                    connect.getInputStream()));
-            response = (JSONObject) in.lines();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+            response = in.readLine();
             in.close();
         } catch (MalformedURLException ex) {
             Logger.getLogger(url.class.getName()).log(Level.SEVERE, null, ex);
@@ -65,7 +86,23 @@ public class url {
         }
         
         // Print the response
-        System.out.println(response);
+        JSONObject outJson = null;
+        try {
+            outJson = (JSONObject) jParse.parse(response);
+        } catch (ParseException ex) {
+            Logger.getLogger(url.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // System.out.println(outJson);
+        
+        // Capture the account number and API token
+        JSONObject subJson = (JSONObject) outJson.get("access");
+        JSONObject tokenJson = (JSONObject) subJson.get("token");
+        apiToken = (String) tokenJson.get("id");
+        JSONObject tenantJson = (JSONObject) tokenJson.get("tenant");
+        acctNum = Integer.valueOf((String)tenantJson.get("id"));
+        System.out.println("Account Number: " + acctNum);
+        System.out.println("API Token: " + apiToken);
     }
+    
     
 }
